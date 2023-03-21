d3566dd36bd21bfeaec69aaa51bc6aee6939744a
liamj1616 <liamj1616@gmail.com>
2023-03-21 18:27:14 -0300

    heroku stuff

--- rideMe/rideMe/settings.py
+++ rideMe/rideMe/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,5 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/postings'
 
-import django_heroku
+
 django_heroku.settings(locals())
